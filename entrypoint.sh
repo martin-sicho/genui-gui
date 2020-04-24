edb846e0f21ca0f4126df8a238314b073caa7709
martin-sicho <sicho.martin@gmail.com>
2020-04-24 12:59:52 +0200

    remove backend files from root

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-python /code/backend/manage.py migrate --noinput
-npm run-script build --prefix /code/frontend
-python /code/backend/manage.py runserver 0.0.0.0:8000
--- set_env_openstack_debug.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-. set_env.sh
-export DOCKER_NET_MTU=1442
