b861365571bf0866d3dd4225c0d2f70e0aeef1af
martin-sicho <sicho.martin@gmail.com>
2020-05-13 16:22:12 +0200

    add a chart with drugex loss to the performance tab

--- src/views/pages/generators/drugex/ModelCards.js
+++ src/views/pages/generators/drugex/ModelCards.js
@@ -1,8 +1,88 @@
 import React from "react"
-import { ModelCard, ModelInfoTab, ModelPerformance } from '../../../../genui';
+import {groupBy, ModelCard, ModelInfoTab, ModelPerformance} from '../../../../genui';
+import {Line} from "react-chartjs-2";
 
-function DrugExPerformanceTab (props) {
+function DrugExAgentPerformanceTab (props) {
+  // TODO: finish this
   return <div>Performance here...</div>
+}
+
+function DrExLossPlot(props) {
+  const losses = groupBy(props.losses, 'extraArgs.isOnValidationSet');
+  if (losses.length === 0) {
+    return <div>No loss data available.</div>
+  }
+  const datasets = losses.map(lossGroup => {
+    if (lossGroup[0].extraArgs.isOnValidationSet) {
+      return {
+        label: "Validation Loss",
+        data: lossGroup.map(loss => loss.value),
+        fill: false,
+        backgroundColor: '#ff6384',
+        borderColor: '#ff6384'
+      }
+    } else {
+      return  {
+        label: "Training Loss",
+        fill: false,
+        data: lossGroup.map(loss => loss.value),
+        backgroundColor: '#36a2eb',
+        borderColor: '#36a2eb'
+      }
+    }
+  });
+  const data = {
+    labels: losses[0].map(loss => loss.extraArgs.step),
+    datasets: datasets
+  };
+  return (
+      <div className="drugex-net-loss-chart">
+        <h4>DrugEx Loss</h4>
+        <Line
+            data={data}
+            options={{
+              scales: {
+                xAxes: [{
+                  scaleLabel: {
+                    display: true,
+                    labelString: 'Step'
+                  }
+                }],
+                yAxes: [
+                  {
+                    scaleLabel: {
+                      display: true,
+                      labelString: 'Loss Value'
+                    }
+                  }
+                ]
+              },
+              title: {
+                display: true,
+                text: 'DrugEx Loss'
+              }}
+            }
+        />
+      </div>
+  );
+}
+
+function DrExSMILESErrorPlot(props) {
+  // TODO: finish this
+  return null;
+}
+
+function DrugExNetworkPerformanceTab(props) {
+
+  const losses = props.getPerfValuesForMetric(props.performance, "ModelPerformanceDrugEx", props.metrics.find(metric => metric.name === 'DrExLoss'));
+  const errors = props.getPerfValuesForMetric(props.performance, "ModelPerformanceDrugEx", props.metrics.find(metric => metric.name === 'SMILES_ER'));
+
+  return (
+      <div className="drugex-performance-plots">
+        <DrExLossPlot losses={losses}/>
+        <DrExSMILESErrorPlot errors={errors}/>
+      </div>
+  )
 }
 
 export class DrugExNetCard extends React.Component {
@@ -33,25 +113,19 @@ export class DrugExNetCard extends React.Component {
     const tabs = [
       {
         title : "Info",
-        renderedComponent : (props) =>
-          <ModelInfoTab
-            {...props}
-            extraTrainingParams={trainingParams}
-            extraValidationParams={validationParams}
-          />
+        renderedComponent : ModelInfoTab
       },
       {
         title: "Performance"
         , renderedComponent : (props) =>
           <ModelPerformance
-            {...props}
-            performanceData={[]} // TODO: fetch performance from server (do this in the tab itself using a url)
-            component={DrugExPerformanceTab}
+              {...props}
+              component={DrugExNetworkPerformanceTab}
           />
       }
     ];
 
-    return <ModelCard {...this.props} tabs={tabs}/>
+    return <ModelCard {...this.props} tabs={tabs} extraTrainingParams={trainingParams} extraValidationParams={validationParams}/>
   }
 }
 
@@ -78,23 +152,18 @@ export class DrugExAgentCard extends React.Component {
     const tabs = [
       {
         title : "Info",
-        renderedComponent : () =>
-          <ModelInfoTab
-            {...this.props}
-            extraTrainingParams={trainingParams}
-          />
+        renderedComponent : ModelInfoTab
       },
       {
         title: "Performance"
-        , renderedComponent : () =>
+        , renderedComponent : (props) =>
           <ModelPerformance
-            {...this.props}
-            performanceData={[]} // TODO: fetch performance from server (do this in the tab itself using a url)
-            component={DrugExPerformanceTab}
+            {...props}
+            component={DrugExAgentPerformanceTab}
           />
       }
     ];
 
-    return <ModelCard {...this.props} tabs={tabs}/>
+    return <ModelCard {...this.props} tabs={tabs} extraTrainingParams={trainingParams}/>
   }
 }
