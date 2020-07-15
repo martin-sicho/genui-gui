4e81ee3e2742d457ed94b0ff8d4a436934d1225d
martin-sicho <sicho.martin@gmail.com>
2020-07-15 15:39:56 +0200

    add support for multipart files

--- src/genui/components/compounds/GenericNewMolSetCard.js
+++ src/genui/components/compounds/GenericNewMolSetCard.js
@@ -4,27 +4,45 @@ import NewMolSetFormRenderer from './NewMolSetFormRenderer';
 
 class GenericNewMolSetCard extends React.Component {
 
-  createMolSetFromFormData = (data) => {
-    data.project = this.props.currentProject.id;
-    if (!data.maxPerTarget) delete data.maxPerTarget; // TODO: remove this and make sure nothing breaks
-    fetch(
-      this.props.molsetListUrl
-      , {
-        method: 'POST'
-        , body: JSON.stringify(data)
-        , headers: {
-          'Content-Type': 'application/json'
-        },
-        credentials: "include",
-      }
-    ).then(response => response.json()).then(
-      data => {
-        this.props.handleCreateNew(this.props.currentMolsetClass, data)
-      }
-    ).catch(
-      (e) => console.log(e)
-    );
-  };
+    createMolSetFromFormData = (data) => {
+        data.project = this.props.currentProject.id;
+
+        // find out if we have a file in the form data
+        // if yes, send a multipart request instead of plain json
+        let isMultiPart = false;
+        const multipartData = new FormData();
+        for (let item in data) {
+            if (data.hasOwnProperty(item)) {
+                const itemData = data[item];
+                if (itemData instanceof File) {
+                    isMultiPart = true;
+                }
+                multipartData.append(item, itemData);
+            }
+        }
+        multipartData.forEach((item, name) => console.log(name, item));
+
+        fetch(
+          this.props.molsetListUrl
+          , {
+            method: 'POST'
+            , body: isMultiPart ? multipartData : JSON.stringify(data)
+            , headers: isMultiPart ?
+                    undefined :
+                    {
+                        'Content-Type': 'application/json'
+                    },
+            credentials: "include",
+          }
+        ).then(response => response.json())
+        .then(data => {
+            console.log(data);
+            this.props.handleCreateNew(this.props.currentMolsetClass, data)
+          }
+        ).catch(
+          (e) => console.log(e)
+        );
+    };
 
   render() {
     return (
