908b5425454e37875ce05b59db26286bd1cadc73
martin-sicho <sicho.martin@gmail.com>
2020-08-03 16:13:28 +0200

    two shorthand components to fetch molecules from backend with their IDs

---
+++ src/genui/components/compounds/MoleculeProviders.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {ComponentWithResources} from "../../index";
+
+export function MoleculeProvider(props) {
+    return (
+        <ComponentWithResources
+            {...props}
+            definition={{
+                mol: new URL(`${props.molID}/`, props.apiUrls.compoundsRoot)
+            }}
+        >
+            {
+                (loaded, data) => loaded ? props.children(data.mol) : <div>Loading molecule data...</div>
+            }
+        </ComponentWithResources>
+    )
+}
+
+export function MoleculeListProvider (props) {
+    const molIDs = props.molIDs;
+    const definition = {};
+    molIDs.forEach(molID => definition[molID] = new URL(`${molID}/`, props.apiUrls.compoundsRoot));
+
+    return (
+        <ComponentWithResources
+            {...props}
+            definition={definition}
+        >
+            {
+                (allLoaded, data) => {
+                    return allLoaded ? props.children(Object.keys(data).map(molID => data[molID])) : <div>Loading molecule data...</div>;
+                }
+            }
+        </ComponentWithResources>
+    )
+}
