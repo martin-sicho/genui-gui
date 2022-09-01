84fdb563492cdd4287bdb3c24ac68e97aa44843e
martin-sicho <sicho.martin@gmail.com>
2022-09-01 11:40:33 +0200

    refactor to create more reusable components

--- src/views/pages/generators/drugex/objective/Modifiers.js
+++ src/views/pages/generators/drugex/objective/Modifiers.js
@@ -91,157 +91,181 @@ function TestChart(props) {
   )
 }
 
-function Clipped(props) {
+function ModifierGrid(props) {
+  return (
+    <div className="modifier-grid">
+      <h2>{props.title}</h2>
+      <CardGrid
+        data={props.data}
+        itemDataComponent={props.itemDataComponent}
+        onDelete={(item) => props.onDelete(props.title, item)}
+      />
+    </div>
+  )
+}
+
+function ModifierForm(props) {
   const [useSliders, setUseSliders] = React.useState(true);
-  const [data, setData] = React.useState({
-    upper: 10,
-    lower: -10,
-    high: 1,
-    low: 0,
-    name: "",
-    project: props.currentProject.id
-  });
-  let [sliderMin, setSliderMin] = React.useState(-20);
-  let [sliderMax, setSliderMax] = React.useState(20);
-  let [sliderStep, setSliderStep] = React.useState(1);
-  if (sliderMax <= sliderMin) {
-    sliderMax = -1 * sliderMin - 1;
-  }
-  if (!sliderStep || sliderStep <= 0) {
-    sliderStep = 1;
-  }
+  const [data, setData] = React.useState(props.initialData);
+  let [sliderMin, setSliderMin] = React.useState(props.sliderMin);
+  let [sliderMax, setSliderMax] = React.useState(props.sliderMax);
+  let [sliderStep, setSliderStep] = React.useState(props.sliderStep);
+  // if (sliderMax <= sliderMin) {
+  //   sliderMax = -1 * sliderMin - 1;
+  // }
+  // if (!sliderStep || sliderStep <= 0) {
+  //   sliderStep = 1;
+  // }
 
   const schema = {
     type: "object",
     properties: {
       name : {type: "string", title: "Name", minLength: 1},
-      upper: {type: "number", title: "Upper", default: data.upper, multipleOf: sliderStep, minimum: sliderMin, maximum: sliderMax},
-      lower: {type: "number", title: "Lower", default: data.lower, multipleOf: sliderStep, minimum: sliderMin, maximum: sliderMax},
-      high: {type: "number", title: "High", default: data.high, multipleOf: sliderStep, minimum: sliderMin, maximum: sliderMax},
-      low: {type: "number", title: "Low", default: data.low, multipleOf: sliderStep, minimum: sliderMin, maximum: sliderMax}
     }
   };
-
-  const inputType = useSliders ? 'range' :'updown';
-  const uiSchema = {
-    upper: {
-        "ui:widget" : inputType
-      },
-    lower: {
-      "ui:widget" : inputType
-    },
-    high: {
-      "ui:widget" : inputType
-    },
-    low: {
-      "ui:widget" : inputType
+  const uiSchema = props.uiSchema;
+  for (const [key, value] of Object.entries(props.schema)) {
+    schema.properties[key] = value;
+    schema.properties[key].default = props.initialData[key];
+    if (value.type === "number") {
+      schema.properties[key].minimum = props.sliderMin;
+      schema.properties[key].maximum = props.sliderMax;
+      uiSchema[key] = {
+        "ui:widget" : useSliders ? 'range' :'updown'
+      };
     }
   }
+
   const inlineStyle = {
     display: 'inline'
   }
   const setInlineWidthStyle = width => ({display: 'inline', width: width});
   return (
+    <div className="modifier-specifier">
+      <Container fluid>
+        <Row>
+          <Col xs="2">
+            <Label for="useSliders">Sliders</Label>
+            <Input
+              name="useSliders"
+              type="checkbox"
+              checked={useSliders}
+              onChange={() => setUseSliders(!useSliders)}
+            />
+          </Col>
+          {
+            useSliders ? (
+              <React.Fragment>
+                <Col xs="2">
+                  <Label for="min" style={inlineStyle}>Min</Label>
+                  <Input
+                    style={setInlineWidthStyle("60%")}
+                    name="min"
+                    type="number"
+                    value={sliderMin}
+                    onChange={(e) => setSliderMin(Number(e.target.value))}
+                  />
+                </Col>
+                <Col xs="2">
+                  <Label for="max" style={inlineStyle}>Max</Label>
+                  <Input
+                    style={setInlineWidthStyle("60%")}
+                    name="max"
+                    type="number"
+                    value={sliderMax}
+                    onChange={(e) => setSliderMax(Number(e.target.value))}
+                  />
+                </Col>
+                <Col xs="2">
+                  <Label for="step" style={inlineStyle}>Step</Label>
+                  <Input
+                    style={setInlineWidthStyle("60%")}
+                    name="step"
+                    type="number"
+                    value={sliderStep}
+                    onChange={(e) => setSliderStep(Number(e.target.value))}
+                  />
+                </Col>
+              </React.Fragment>
+            ) : null
+          }
+        </Row>
+        <Row>
+          <Col xs="6"><hr/></Col>
+        </Row>
+        <Row>
+          <Col xs="6">
+            <Form schema={schema}
+                  formData={data}
+                  uiSchema={uiSchema}
+              // liveValidate
+                  showErrorList={false}
+                  onChange={data => setData(data.formData)}
+                  onSubmit={data => props.onAdd(props.title, data.formData)}
+                  onError={data => console.log(data)}
+            >
+              <Button type="submit" color="primary">Create</Button>
+            </Form>
+          </Col>
+          <Col xs="6">
+            <TestChart
+              data={{
+                upper: data.upper,
+                lower: data.lower,
+                high: data.high,
+                low: data.low,
+                smooth: data.smooth}
+              }
+              url={props.url}
+              min={sliderMin}
+              max={sliderMax}
+              step={sliderStep}
+              title={props.title}
+            />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  )
+}
+
+function Clipped(props) {
+  const initialData = {
+    upper: 10,
+    lower: -10,
+    high: 1,
+    low: 0,
+    name: "",
+    smooth: false,
+    project: props.currentProject.id
+  }
+  const schema = {
+    upper: {type: "number", title: "Upper"},
+    lower: {type: "number", title: "Lower"},
+    high: {type: "number", title: "High"},
+    low: {type: "number", title: "Low"},
+    smooth: {type: "boolean", title: "Smooth", enum: [true, false]}
+  };
+  const uiSchema = {
+    smooth : {
+      "ui:widget": "select"
+    }
+  }
+
+  return (
     <React.Fragment>
-      <h2>{props.title}</h2>
-      <CardGrid
-        data={props.data}
-        itemDataComponent={
-          (props) => (
-            <React.Fragment>
-              <em>Upper:</em> {props.item.upper}<br/>
-              <em>Lower:</em> {props.item.lower}<br/>
-              <em>High:</em> {props.item.high}<br/>
-              <em>Low:</em> {props.item.low}
-            </React.Fragment>
-          )
-        }
-        onDelete={(item) => props.onDelete(props.title, item)}
-      />
-      <div className="modifier-specifier">
-        <Container fluid>
-          <Row>
-            <Col xs="2">
-              <Label for="useSliders">Sliders</Label>
-              <Input
-                name="useSliders"
-                type="checkbox"
-                checked={useSliders}
-                onChange={() => setUseSliders(!useSliders)}
-              />
-            </Col>
-            {
-              useSliders ? (
-                <React.Fragment>
-                  <Col xs="2">
-                    <Label for="min" style={inlineStyle}>Min</Label>
-                    <Input
-                      style={setInlineWidthStyle("60%")}
-                      name="min"
-                      type="number"
-                      value={sliderMin}
-                      onChange={(e) => setSliderMin(Number(e.target.value))}
-                    />
-                  </Col>
-                  <Col xs="2">
-                    <Label for="max" style={inlineStyle}>Max</Label>
-                    <Input
-                      style={setInlineWidthStyle("60%")}
-                      name="max"
-                      type="number"
-                      value={sliderMax}
-                      onChange={(e) => setSliderMax(Number(e.target.value))}
-                    />
-                  </Col>
-                  <Col xs="2">
-                    <Label for="step" style={inlineStyle}>Step</Label>
-                    <Input
-                      style={setInlineWidthStyle("60%")}
-                      name="step"
-                      type="number"
-                      value={sliderStep}
-                      onChange={(e) => setSliderStep(Number(e.target.value))}
-                    />
-                  </Col>
-                </React.Fragment>
-              ) : null
-            }
-          </Row>
-          <Row>
-            <Col xs="6"><hr/></Col>
-          </Row>
-          <Row>
-            <Col xs="6">
-              <Form schema={schema}
-                    formData={data}
-                    uiSchema={uiSchema}
-                    // liveValidate
-                    showErrorList={false}
-                    onChange={data => setData(data.formData)}
-                    onSubmit={data => props.onAdd(props.title, data.formData)}
-                    onError={data => console.log(data)}
-              >
-                <Button type="submit" color="primary">Create</Button>
-              </Form>
-            </Col>
-            <Col xs="6">
-              <TestChart
-                data={{
-                  upper: data.upper,
-                  lower: data.lower,
-                  high: data.high,
-                  low: data.low}
-                }
-                url={props.url}
-                min={sliderMin}
-                max={sliderMax}
-                step={sliderStep}
-                title={props.title}
-              />
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <ModifierGrid {...props} itemDataComponent={
+        (props) => (
+          <React.Fragment>
+            <em>Upper:</em> {props.item.upper}<br/>
+            <em>Lower:</em> {props.item.lower}<br/>
+            <em>High:</em> {props.item.high}<br/>
+            <em>Low:</em> {props.item.low}<br/>
+            <em>Smooth:</em> {props.item.smooth ? "Yes" : "No"}
+          </React.Fragment>
+        )
+      }/>
+      <ModifierForm {...props} initialData={initialData} schema={schema} uiSchema={uiSchema} sliderStep={1} sliderMin={-20} sliderMax={20}/>
     </React.Fragment>
   );
 }
